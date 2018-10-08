048814e1c311e8842fa71bf9ddeb8e6769d49242
slick-solutions <36201692+ASTR0NAUTJ0NES@users.noreply.github.com>
2018-10-08 12:41:49 -0400

    first commit

---
+++ HelloAll.java
@@ -0,0 +1,14 @@
+/**
+ * HelloAll - Lab 1-2.
+ * 
+ * @author Andrew Peyton Albanese 
+ * @version 2016-01-18
+ */
+public class HelloAll
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, All!");
+    }
+}
